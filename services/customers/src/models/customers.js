3329c795c7193ada8a538d8e2081f9de0984e87a
Eniola Osabiya <eosabiya@gmail.com>
2020-06-18 00:12:52 +0100

    Revert "Customer service"

--- services/customers/src/models/customers.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-
-customerSchema=mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: String || undefined,
-    phone: String || undefined
-});
-
-module.exports=mongoose.model('customers',customerSchema);
